2500411b93262ebdb454f7236d90f9ad4564bcc7
4l0nsoo <di4zalonsoj@gmail.com>
2024-11-11 01:02:50 -0500

    trying to add ghP succesfully#1

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "https://4l0nsoo.github.io/TourDotHTML"
+  base: "https://4l0nsoo.github.io/TourdotHTML"
 })
